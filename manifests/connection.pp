504ebb7f959f06525bedd407be5e739e0a62e6e6
James Fryman <james@frymanet.com>
2012-06-06 17:15:24 -0500

    WIP: Adding service restart to connection.pp

--- manifests/connection.pp
+++ manifests/connection.pp
@@ -24,5 +24,6 @@ define openswan::connection(
   file { "/etc/ipsec.d/connections/${name}.conf":
     ensure  => file,
     content => template('openswan/connection.conf.erb'),
+    notify  => Class['openswan::service'],
   }
 }
