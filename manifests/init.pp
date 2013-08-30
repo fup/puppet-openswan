0c944310db6df6b39c7f1c47f03c2bacfe9f3cac
Ib Lundgren <ib.lundgren@gmail.com>
2013-08-30 10:08:56 +0100

    Add more explicit config and connection options.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,9 @@ class openswan (
   $debug_level              = $openswan::params::debug_level,
   $nat_t                    = $openswan::params::nat_t,
   $opportunistic_encryption = $openswan::params::opportunistic_encryption,
+  $dumpdir                  = $openswan::params::dumpdir,
   $plutoopts                = $openswan::params::plutoopts,
+  $plutostderrlog           = $openswan::params::plutostderrlog,
   $protostack               = $openswan::params::protostack,
   $virtual_private          = $openswan::params::virtual_private
 ) inherits openswan::params {
@@ -16,7 +18,9 @@ class openswan (
     nat_t                    => $nat_t,
     opportunistic_encryption => $opportunistic_encryption,
     protostack               => $protostack,
+    dumpdir                  => $dumpdir,
     plutoopts                => $plutoopts,
+    plutostderrlog           => $plutostderrlog,
     virtual_private          => $virtual_private,
   }
   ~> class { 'openswan::service': }
